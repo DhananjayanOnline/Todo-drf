e513e49c255a37a80a168fb427f8548eb60f098f
Dhananjayan <dhananjayan.online@outlook.com>
2022-12-08 13:04:38 +0530

    forms.py added, urls.py created

--- todo/urls.py
+++ todo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from api.views import TodoView, TodosModelViews, UsersView
@@ -47,5 +47,6 @@ router.register("api/v1/users", UsersView, basename="users")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-      path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("web/", include("todoweb.urls")),
 ]+router.urls
--- todoweb/views.py
+++ todoweb/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from todoweb.forms import UserRegistrationForm
 
 # Create your views here.
+
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        form = UserRegistrationForm()
+        return render(request, "register.html", {"form":form})
 
