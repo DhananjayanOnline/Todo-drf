8475529ed8057921ca056bde0caf3cabc807627a
Dhananjayan <dhananjayan.online@outlook.com>
2022-12-13 14:33:51 +0530

    login authentication added, todo-list started

--- todoweb/urls.py
+++ todoweb/urls.py
@@ -4,4 +4,6 @@ from todoweb import views
 urlpatterns=[
     path("register", views.RegisterView.as_view(), name="register"),
     path("login", views.LoginView.as_view(), name="signin"),
+    path("home", views.IndexView.as_view(), name='home'),
+    path("todos/all", views.TodoListView.as_view(), name='todo-list'),
 ]   
--- todoweb/views.py
+++ todoweb/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from todoweb.forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
+from api.models import Todo
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -23,3 +25,26 @@ class LoginView(View):
         form = UserLoginForm()
         return render(request, "login.html", {"form":form})
 
+    def post(self, request, *args, **kwargs):
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            uname = form.cleaned_data.get('username')
+            pwd = form.cleaned_data.get('password')
+            user = authenticate(request, username=uname, password=pwd)
+            if user:
+                login(request, user)
+                return redirect('home')
+            else:
+                print('invalid user')
+                return redirect('signin')
+
+class IndexView(TemplateView):
+    # def get(self, request, *args, **kwargs):
+    #     return render(request, 'index.html')
+    template_name = 'index.html'
+
+
+class TodoListView(View):
+    def get(self, request, *args, **kwargs):
+        user_todos = Todo.objects.filter(user=request.user)
+        return render(request, 'todo-list.html', {"todos":user_todos})
