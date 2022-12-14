4244d92d3f8a0cc01b1a2b8bb700e96e311c534b
Dhananjayan <dhananjayan.online@outlook.com>
2022-12-14 13:24:32 +0530

    todo form created

--- todoweb/forms.py
+++ todoweb/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from api.models import Todo
 
 class UserRegistrationForm(forms.ModelForm):
     class Meta:
@@ -24,3 +24,14 @@ class UserRegistrationForm(forms.ModelForm):
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['task_name']
+
+        widgets = {
+            "task_name":forms.TextInput(attrs={"class":"form-input"})
+        }
+
